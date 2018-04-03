b58ea55886bb0a767de64184526c9534a81b3165
Zach Goldman <ZGoldman@protonmail.com>
2018-04-02 18:45:51 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,8 +6,6 @@ const app = express();
 
 app.use((req, res, next) => {
     // write your logging code here
-
-    //var logs = (req.headers["user-agent"] + new Date() + req.method + req.path + "HTTP/" + req.httpVersion + res.statusCode);
     var agent = req.headers["user-agent"].replace(',', '');
     var time = new Date().toISOString();
     var method = req.method;
@@ -36,7 +34,6 @@ app.get("/logs", function (req, res) {
     var headers;
     var jsonArray;
     var jsonObj = [];
-    // Read the log.csv file using 'fs'
         fs.readFile(csvFilePath, (err, data) => {
         if (err) throw err;
             bufferString = data.toString();
@@ -52,11 +49,6 @@ app.get("/logs", function (req, res) {
             }
             res.send(jsonObj);
       });
-      
-    // Go through the info line by line (except the first row)
-    // Prepare your json payload and send it.
-    // let jsonData = [];
-    // res.json(jsonData);
 });
 
 
